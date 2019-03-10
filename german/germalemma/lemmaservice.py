f209777c4842c715ace60b9c936527c4aa566bae
andymur <andymurh@gmail.com>
2019-03-10 19:51:19 +0100

    now we can normalize words (at least try to).

--- german/germalemma/lemmaservice.py
+++ german/germalemma/lemmaservice.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 from germalemma import GermaLemma
+import json
+from flask import request, Response
 import pickle
 from ClassifierBasedGermanTagger.ClassifierBasedGermanTagger import ClassifierBasedGermanTagger
 from flask import Flask
+from pattern.de import singularize, conjugate, predicative
+
 app = Flask(__name__)
 
 lemmatizer = GermaLemma()
@@ -10,11 +14,15 @@ lemmatizer = GermaLemma()
 with open('data/pos.pickle', 'rb') as f:
         tagger = pickle.load(f)
 
-@app.route('/')
-def hello_world():
-    pos = tagger.tag(['Wand', u'Wände', u'Wänden'])
+@app.route('/normalize/<word>')
+def hello_world(word):
+    pos = tagger.tag([word])
     print(pos)
+    content = []
     for item in pos:
         w, p = item
-        print(lemmatizer.find_lemma(w, p))
-    return 'Hello, World!'
+        lemma = lemmatizer.find_lemma(w, p)
+        content.append(lemma)
+
+    response = Response(response=json.dumps(content), status=200, mimetype='application/json')    
+    return response

8a698d58be3d24aac01f6752afd4c55e1b4d0c38
andymur <andymurh@gmail.com>
2019-07-29 21:12:01 +0200

    Add wortart buttons

--- remarker/src/components/MainComponent.vue
+++ remarker/src/components/MainComponent.vue
@@ -2,11 +2,20 @@
   <div>
     <div @click="gotoWord('prev')">&lt;&mdash;</div>
     <div @click="gotoWord('next')">&mdash;&gt;</div>
-    <input type="text" v-model="currentWordOfModel" />
-    <input type="text" :value="currentWordOfModel" />
+    <div id="word-editors">
+      <input type="text" v-model="currentWordOfModel" />
+      <input type="text" :value="currentWordOfModel" />
+    </div>
+    <div id="wortart-buttons">
+      <input type="button" value="Substantiv" v-bind:class="selectedWordArt(currentWord, 'substantiv')" />
+      <input type="button" value="Verb" v-bind:class="selectedWordArt(currentWord, 'verb')"/>
+      <input type="button" value="Adjektiv" v-bind:class="selectedWordArt(currentWord, 'adjektiv')"/>
+      <input type="button" value="Pronomenon" v-bind:class="selectedWordArt(currentWord, 'pronomenon')"/>
+      <input type="button" value="Präposition" v-bind:class="selectedWordArt(currentWord, 'präposition')"/>
+    </div>
     <div @click="gotoWord('prev')">&lt;&mdash;</div>
     <div @click="gotoWord('next')">&mdash;&gt;</div>
-    <textarea v-model="wordsModel"></textarea>
+    <textarea v-model="wordsList"></textarea>
   </div>
   
 </template>
@@ -27,24 +36,39 @@ export default {
     console.log("We're in the component");
     this.currentWordOfModel = this.currentWord == undefined ? this.words[0] : this.currentWord;
     var wordData = "";
+
     this.words.forEach(element => {
       wordData += element + "\n"
+      this.wordsModel[element] = element == 'schatz' ? {'wordart': 'substantiv'} : {'wordart': 'verb'}
     });
-    this.wordsModel = wordData;
+    this.wordsList = wordData;
   },
+  data: function() {
+    return {
+      wordsList: "",
+      currentWordOfModel: null,
+      wordsModel: {}
+    }
+  },
+  computed: {    
+  },  
   methods: {
+    selectedWordArt: function(selectedWord, wordart) {
+      
+      return {'selected': this.wordsModel[selectedWord]['wordart'] == wordart};
+    },
     'gotoWord': function(direction) {
       var word = null;
       var wordCount = this.words.length;
 
-      var currentIndex = this.words.indexOf(this.currentWordOfModel);
-
+      var currentIndex = this.words.indexOf(this.currentWord);
+      
       if (currentIndex == -1) {
         return;
       }
 
       if (direction == 'next') {
-        if (currentIndex >= wordCount) {
+        if (currentIndex >= wordCount - 1) {
           return;
         }
         word = this.words[currentIndex + 1];
@@ -57,18 +81,16 @@ export default {
       this.currentWordOfModel = word;
       this.$router.push({ name: 'word-edit', params: { word: word }})
     }
-  },
-  data: function() {
-    return {
-      wordsModel: "",
-      currentWordOfModel: null
-    }
-  }
+  }  
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+input.selected {
+  font-size: 18pt;
+  color:red;
+}
 h3 {
   margin: 40px 0 0;
 }
